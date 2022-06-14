0c10eb59696e5067626d27ce7ff1095eb186145d
Toi <c0a211320d@edu.teu.ac.jp>
2022-06-14 20:43:49 +0900

    詳細

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,30 +1,31 @@
 import random
+
 def main():
-    shutsudai()
-    
+    seikai = shutsudai()
+    kaitou(seikai)
+
 def shutsudai():
-    Q = [0, 1, 2]
-    A1 = ["マスオ", "ますお"]
-    A2 = ["ワカメ", "わかめ"]
-    A3 = ["甥","おい", "甥っ子", "おいっこ"]
-    if random.choice(Q) == 0:
-        input("サザエさんの旦那の名前は？")
-        a = input("答えるんだ：")
-        if a == A1:
-            print("正解！！！")
-        else:
-            print("出直してこい")
-    elif random.choice(Q) == 1:
-        input("カツオの妹の名前は？")
-        a = input("答えるんだ：")
-        if a == A2:
-            print("正解！！！")
-        else:
-            print("でなおしてこい")
-    elif random.choice(Q) == 2:
-        input("タラオはカツオから見てどんな関係？")
-        a = input("答えるんだ：")
-        if a == A3:
-            print("正解！！！")
-        else:
-            print("出直してこい")
+    Q = [
+         {"q":"サザエさんの旦那の名前は？","a":["マスオ", "ますお"]},
+         {"q": "カツオの妹の名前は？","a":["ワカメ", "わかめ"]}
+         {"q": "タラオはカツオから見てどんな関係？", "a":["甥", "おい", "甥っ子","おいっこ"]},
+        ]
+    A0 = ["マスオ", "ますお"]
+    A1 = ["ワカメ", "わかめ"]
+    A2 = ["甥","おい", "甥っ子", "おいっこ"]
+    print("問題：")
+    r = random.randint(0,2)
+    print(Q[r]["q"])
+    return Q[r]["a"]
+
+def kaitou(seikai):
+    a = input("答えるんだ：")
+    if a in seikai:
+        print("正解！！！")
+    else:
+        print("出直してこい")
+
+if __name__ == "__main__":
+    main()
+
+    
