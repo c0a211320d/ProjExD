4c137e3997e3ead176c13097c93d158e2d2ff42b
Toi <c0a211320d@edu.teu.ac.jp>
2022-06-21 14:46:07 +0900

    問３

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,7 +1,12 @@
 
 import tkinter as tk
+import tkinter.messagebox as tkm
 
 if __name__ == "__main__":
+    def button_click(event):
+        btn = event.widget
+        num = btn["text"]
+        tkm.showinfo("", f"{num}のボタンがクリックされました")
     root = tk.Tk()
     root.title("電卓")
     root.geometry("300x500")
@@ -14,6 +19,7 @@ if __name__ == "__main__":
                                 height="2", 
                                 font=("Times New Roman", 30)
                                 )
+        btn.bind("<1>", button_click)
         btn.grid(row=r, column=c)
         c += 1
         if (i-1) % 3 == 0:
