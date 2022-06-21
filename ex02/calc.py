a2eaddf7026b0bb50833d7d81e7fb45d6c0d1d1c
Toi <c0a211320d@edu.teu.ac.jp>
2022-06-21 15:24:34 +0900

    問６まで

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,28 +2,38 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-if __name__ == "__main__":
-    def button_click(event):
+def button_click(event):
         btn = event.widget
         num = btn["text"]
-        tkm.showinfo("", f"{num}のボタンがクリックされました")
+        #tkm.showinfo("", f"{num}のボタンがクリックされました")
+        entry.insert(tk.END, i)
+
+if __name__ == "__main__":
     root = tk.Tk()
     root.title("電卓")
-    root.geometry("300x500")
+    #root.geometry("300x600")
 
-    r, c = 0, 0
-    for i in range(9, -1, -1):
-        btn = button = tk.Button(root, 
-                                text=f"{i}", 
+    entry = tk.Entry(root,
+                    justify = "right",
+                    width = "10",
+                    font = ("Time New Roman" , 40)
+                    )
+    entry.grid(row=0, column=0, columnspan=3)
+
+    r = 1;c = 0
+    for num,i in enumerate([9,8,7,6,5,4,3,2,1,0,"+"]):
+        button = tk.Button(root, 
+                                text=i, 
                                 width="4", 
                                 height="2", 
                                 font=("Times New Roman", 30)
                                 )
-        btn.bind("<1>", button_click)
-        btn.grid(row=r, column=c)
+        button.bind("<1>", button_click)
+        button.grid(row=r, column=c)
         c += 1
-        if (i-1) % 3 == 0:
+        if (num+1) % 3 == 0:
             r += 1
             c = 0
+    
 
     root.mainloop()
