dbcc1074991d80f3f0d9ee0eabfee8959ed27668
Toi <c0a211320d@edu.teu.ac.jp>
2022-06-28 14:33:56 +0900

    問5

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+    print(f"{key}が押されました")
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -9,4 +14,7 @@ if __name__ == "__main__":
     tori = tk.PhotoImage(file="fig/0.png")
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image=tori, tag="tori")
+
+    key = ""
+    root.bind("<KeyPress>", key_down)
     root.mainloop()
