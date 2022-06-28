b704ea8337e7eb57010111e44a53091f05b054cb
Toi <c0a211320d@edu.teu.ac.jp>
2022-06-28 17:26:12 +0900

    修正１

--- ex03/maze.py
+++ ex03/maze.py
@@ -34,24 +34,18 @@ def main_proc():
     canvas.coords("tori", cx, cy)
     root.after(100, main_proc)
     
-
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
-    
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
-    
     canvas.pack()
     maze_bg = mm.make_maze(15, 9)
     mm.show_maze(canvas, maze_bg)
-
     #print(maze_bg)
-
     tori = tk.PhotoImage(file="fig/0.png")
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50
     canvas.create_image(cx, cy, image=tori, tag="tori")
-
     key = ""
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
