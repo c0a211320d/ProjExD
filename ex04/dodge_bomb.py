1e3802984d61265015fa05d3d4ad58ede8818b05
Toi <c0a211320d@edu.teu.ac.jp>
2022-07-05 14:37:03 +0900

    問4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -19,12 +19,26 @@ def main():
 
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
-        screen_sfc.blit(kkimg_sfc, kkimg_rct)
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-            pg.display.update()
-            clock.tick(1000)
+           
+        key_states = pg.key.get_pressed()   #辞書
+        if key_states[pg.K_UP] == True:
+            kkimg_rct.centery -= 1
+        if key_states[pg.K_DOWN] == True:
+            kkimg_rct.centery += 1
+        if key_states[pg.K_LEFT] == True:
+            kkimg_rct.centerx -= 1
+        if key_states[pg.K_RIGHT] == True:
+            kkimg_rct.centerx += 1
+        
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
+        pg.display.update()
+        clock.tick(1000)
+
+
 
 
 if __name__ == "__main__":
