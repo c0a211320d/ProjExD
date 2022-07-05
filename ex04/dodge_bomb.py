bf3162be6ccef41d489943fd674d70c1d4d5e51f
Toi <c0a211320d@edu.teu.ac.jp>
2022-07-05 14:26:35 +0900

    問3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -12,9 +12,14 @@ def main():
     pg.display.update()
     clock.tick(0.5)
 
+    kkimg_sfc = pg.image.load("fig/6.png")
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
+    kkimg_rct = kkimg_sfc.get_rect()
+    kkimg_rct.center = 900, 400
+
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rct)
-
+        screen_sfc.blit(kkimg_sfc, kkimg_rct)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
